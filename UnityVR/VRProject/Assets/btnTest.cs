9863f6dc4208e50ecda48848f5711648d79d7402
John__Wu <2293853581@qq.com>
2021-10-04 19:40:43 +0800

    UnityVR

---
+++ UnityVR/VRProject/Assets/btnTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BtnOnclick()
+	{
+		Debug.Log("按钮被点击");
+	}
+}
